d152f3d5db70f61e8593de459751ea85fcc173c1
Victória Lima <victoriajhaniny@icloud.com>
2026-03-22 14:33:28 -0300

    Revert "Hello Word"
    This reverts commit 4a03c1a54bd4a990909fc3677436d1cfadfaf616.

--- GitHub Desktop Exemplo/src/github/desktop/exemplo/GitHubDesktopExemplo.java
+++ GitHub Desktop Exemplo/src/github/desktop/exemplo/GitHubDesktopExemplo.java
@@ -1,10 +1,20 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
 package github.desktop.exemplo;
 
+/**
+ *
+ * @author viiic
+ */
 public class GitHubDesktopExemplo {
 
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        System.out.println("Hello Word!");
+        // TODO code application logic here
     }
     
 }
